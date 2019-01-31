0b2eb2cd0a4b3722761d3b4d1d9545aa09e2dd2a
Jesse Luoto <jesse.luoto@gmail.com>
2019-01-31 09:52:47 +0200

    Add clunky c++ based ray tracing

--- convert.js
+++ convert.js
@@ -1,5 +1,5 @@
 const { readFileSync, writeFileSync } = require("fs");
-var wabt = require("wabt")();
+/*var wabt = require("wabt")();
 
 const inputWat = "main.wat";
 const outputWasm = "main.wasm";
@@ -7,9 +7,8 @@ const outputWasm = "main.wasm";
 const wasmModule = wabt.parseWat(inputWat, readFileSync(inputWat, "utf8"));
 const { buffer } = wasmModule.toBinary({});
 
-const base = Buffer.from(buffer).toString("base64");
-
-//const base = Buffer.from(readFileSync("./main.wasm")).toString("base64");
+const base = Buffer.from(buffer).toString("base64");*/
+const base = Buffer.from(readFileSync("./main.wasm")).toString("base64");
 writeFileSync(
   "./source.js",
   readFileSync("./source.js")
--- src.js
+++ src.js
@@ -1,18 +1,18 @@
 var b = "WASM_HERE";
-
+var SIZE = 500;
 var r = (x, y, color) => {
-  c.fillStyle =
-    "#" + color.toString(16) + color.toString(16) + color.toString(16);
+  cc = Math.floor(color).toString(16);
+  c.fillStyle = "#" + cc + cc + cc;
   c.fillRect(x, y, 1, 1);
 };
 var Wasm = WebAssembly;
 
 Wasm.compile(Uint8Array.from(atob(b), c => c.charCodeAt(0)))
-  .then(compiled => Wasm.instantiate(compiled))
+  .then(Wasm.instantiate)
   .then(m => {
-    for (let x = 0; x < 256; x++) {
-      for (let y = 0; y < 256; y++) {
-        r(x, y, m.exports.r(x, y));
+    for (let x = 0; x < SIZE; x++) {
+      for (let y = 0; y < SIZE; y++) {
+        r(x, y, m.exports._r(x, y));
       }
     }
   });
