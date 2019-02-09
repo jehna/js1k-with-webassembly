3a283849376c55537f91d45b6199bb992b4440ce
Jesse Luoto <jesse.luoto@gmail.com>
2019-02-09 21:55:13 +0200

    Add new, custom math functions

--- main.cpp
+++ main.cpp
@@ -1,58 +1,5 @@
-#include <limits>
-#include <cmath>
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include "geometry.h"
+#include "vector3.cpp"
 #include <emscripten/emscripten.h>
-
-struct Sphere
-{
-  Vec3f center;
-  float radius;
-
-  Sphere(const Vec3f &c, const float &r) : center(c), radius(r) {}
-
-  bool ray_intersect(const Vec3f &orig, const Vec3f &dir, float &t0) const
-  {
-    Vec3f L = center - orig;
-    float tca = L * dir;
-    float d2 = L * L - tca * tca;
-    if (d2 > radius * radius)
-      return false;
-    float thc = sqrtf(radius * radius - d2);
-    t0 = tca - thc;
-    float t1 = tca + thc;
-    if (t0 < 0)
-      t0 = t1;
-    if (t0 < 0)
-      return false;
-    return true;
-  }
-};
-
-struct Light
-{
-  Light(const Vec3f &p, const float &i) : position(p), intensity(i) {}
-  Vec3f position;
-  float intensity;
-};
-
-float cast_ray(const Vec3f &orig, const Vec3f &dir, const Sphere &sphere, const Light &light)
-{
-
-  float sphere_dist = std::numeric_limits<float>::max();
-  if (!sphere.ray_intersect(orig, dir, sphere_dist))
-  {
-    return 255.; // background color
-  }
-
-  Vec3f point = orig + dir * sphere_dist;
-  Vec3f light_dir = (light.position - point).normalize();
-  Vec3f N = (point - sphere.center).normalize();
-  float diffuse_light_intensity = light.intensity * std::max(0.f, light_dir * N);
-  return diffuse_light_intensity * 255.;
-}
 
 #ifdef __cplusplus
 extern "C"
@@ -62,15 +9,9 @@ extern "C"
   float EMSCRIPTEN_KEEPALIVE
   r(int j, int i)
   {
-    Sphere sphere = Sphere(Vec3f(-3, 0, -16), 8);
-    Light light = Light(Vec3f(-20, 20, 20), 1.);
-    const int fov = 300;
-    const int width = 500;
-    const int height = 500;
-    float x = (2 * (i + 0.5) / (float)width - 1) * tan(fov / 2.) * width / (float)height;
-    float y = -(2 * (j + 0.5) / (float)height - 1) * tan(fov / 2.);
-    Vec3f dir = Vec3f(x, y, -1).normalize();
-    return cast_ray(Vec3f(0, 0, 0), dir, sphere, light) * 1.;
+    Vector3 foo = Vector3::up;
+    Vector3 bar = {0, 0, 0};
+    return (foo + bar).y;
   }
 
 #ifdef __cplusplus
---
+++ vector3.cpp
@@ -0,0 +1,76 @@
+#include <math.h>
+
+struct Vector3
+{
+  float x;
+  float y;
+  float z;
+
+  static Vector3 up;
+  static Vector3 zero;
+
+  Vector3 operator+(const Vector3 &value) const
+  {
+    Vector3 result = {
+        x + value.x,
+        y + value.y,
+        z + value.z};
+    return result;
+  };
+
+  Vector3 operator-(const Vector3 &value) const
+  {
+    Vector3 result = {
+        x - value.x,
+        y - value.y,
+        z - value.z};
+    return result;
+  };
+
+  float length()
+  {
+    return sqrt(x * x + y * y + z * z);
+  };
+};
+
+Vector3 Vector3::up = {0., 1., 0.};
+Vector3 Vector3::zero = {0., 0., 0.};
+
+float dotProduct(Vector3 a, Vector3 b)
+{
+  return (a.x * b.x) + (a.y * b.y) + (a.z * b.z);
+};
+
+Vector3 crossProduct(Vector3 a, Vector3 b)
+{
+  Vector3 result =
+      {(a.y * b.z) - (a.z * b.y),
+       (a.z * b.x) - (a.x * b.z),
+       (a.x * b.y) - (a.y * b.x)};
+  return result;
+};
+
+Vector3 scale(Vector3 value, float scale)
+{
+  Vector3 result = {
+      value.x * scale,
+      value.y * scale,
+      value.z * scale};
+  return result;
+};
+
+float length(Vector3 value)
+{
+  return sqrt(dotProduct(value, value));
+};
+
+Vector3 normalize(Vector3 value)
+{
+  return scale(value, 1. / length(value));
+};
+
+Vector3 reflectThrough(Vector3 value, Vector3 normal)
+{
+  Vector3 direction = scale(normal, dotProduct(value, normal));
+  return scale(direction, 2) - value;
+};
