20a09553d86428da2a9dbdf3c833e8e298a53611
Jesse Luoto <jesse.luoto@gmail.com>
2019-02-10 16:44:43 +0200

    Remove base64 encoding
    Save bytes with template strings!

--- convert.js
+++ convert.js
@@ -8,7 +8,7 @@ const wasmModule = wabt.parseWat(inputWat, readFileSync(inputWat, "utf8"));
 const { buffer } = wasmModule.toBinary({});
 
 const base = Buffer.from(buffer).toString("base64");*/
-const base = Buffer.from(readFileSync("./main.wasm")).toString("base64");
+const base = Buffer.from(readFileSync("./main.wasm")).toString("utf-8");
 writeFileSync(
   "./source.js",
   readFileSync("./source.js")
--- src.js
+++ src.js
@@ -1,13 +1,11 @@
-var b = "WASM_HERE";
+var b = `WASM_HERE`;
 var SIZE = 500;
 var Wasm = WebAssembly;
 
-Wasm.instantiateStreaming(fetch("data:application/wasm;base64," + b)).then(
-  a => {
-    let imageData = new ImageData(SIZE, SIZE);
-    for (let index = 0; index < SIZE * SIZE * 4; index++) {
-      imageData.data[index] = a.instance.exports._r(index);
-    }
-    c.putImageData(imageData, 0, 0);
+Wasm.instantiateStreaming(fetch("data:application/wasm," + b)).then(a => {
+  let imageData = new ImageData(SIZE, SIZE);
+  for (let index = 0; index < SIZE * SIZE * 4; index++) {
+    imageData.data[index] = a.instance.exports._r(index);
   }
-);
+  c.putImageData(imageData, 0, 0);
+});
