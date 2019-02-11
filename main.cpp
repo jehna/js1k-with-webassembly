bfd4d185e3599bd294b95cf22b0a5d6237c138f8
Jesse Luoto <jesse.luoto@gmail.com>
2019-02-12 01:20:11 +0200

    Add noise function

--- main.cpp
+++ main.cpp
@@ -46,9 +46,9 @@ struct Camera
 };
 
 Camera Camera::main = {
-    .position = {10, 10, 10},
+    .position = {0, 30, 0},
     .fieldOfView = 45.,
-    .direction = {0, 5, 0}};
+    .direction = {10, 25, 10}};
 
 struct Sphere
 {
@@ -66,20 +66,89 @@ struct Hit
   int sphereIndex;
 };
 
-float surfaceHeightAtPoint(const float x, const float y)
+static int lookupTable[] = {74, 23, 40, 35, 27, 15, 42, 8, 29, 7, 19, 3, 28, 18, 13, 17, 77, 94, 76, 16, 83, 72, 31, 9, 91, 22, 2, 85, 20, 1, 89, 95, 87, 26, 92, 30, 80, 103, 125, 88, 119, 4, 69, 124, 86, 108, 24, 112, 115, 106, 6, 123, 10, 25, 55, 60, 113, 73, 61, 51, 122, 107, 5, 96, 64, 0, 90, 34, 110, 59, 14, 97, 65, 11, 12, 120, 67, 46, 126, 104, 50, 53, 98, 56, 38, 36, 105, 62, 49, 109, 45, 66, 52, 44, 58, 127, 71, 63, 68, 82, 114, 57, 79, 99, 118, 48, 100, 81, 37, 101, 33, 84, 70, 75, 78, 54, 41, 47, 117, 43, 102, 116, 39, 111, 121, 21, 32, 93};
+const int tableLength = sizeof(lookupTable) / sizeof(int);
+
+int lookup(int index)
 {
-  return sin(x) + sin(y);
+  return lookupTable[index % tableLength];
 }
 
-const float eps = 0.1;
+float fade(const float value)
+{
+  return value * value * value * (value * (value * 6 - 15) + 10);
+}
+
+float grad(int hash, float x, float y)
+{
+  switch (hash & 3)
+  {
+  case 0:
+    return x + y;
+  case 1:
+    return -x + y;
+  case 2:
+    return x - y;
+  case 3:
+    return -x - y;
+  default:
+    return 0; // never happens
+  }
+}
+
+float lerp(float from, float to, float percent)
+{
+  return from + percent * (to - from);
+}
+
+float noise(const float x, const float y)
+{
+
+  int xi = (int)x;
+  int yi = (int)y;
+  float xx = x - xi;
+  float yy = y - yi;
+  float fx = fade(xx);
+  float fy = fade(yy);
+
+  int aa = lookup(lookup(xi) + yi);
+  int ab = lookup(lookup(xi) + yi + 1);
+  int ba = lookup(lookup(xi + 1) + yi);
+  int bb = lookup(lookup(xi + 1) + yi + 1);
+
+  float x1 = lerp(grad(aa, xx, yy),
+                  grad(ba, xx - 1, yy),
+                  fx);
+  float x2 = lerp(grad(ab, xx, yy - 1),
+                  grad(bb, xx - 1, yy - 1),
+                  fx);
+  float value = lerp(x1, x2, fy);
+
+  return (value + 1) / 2;
+}
+
+float coolnoise(float x, float y)
+{
+  return noise(x / 20, y / 20) +
+         noise(x / 10, y / 10) / 2 +
+         noise(x / 5, y / 5) / 6 +
+         noise(x / 2, y / 2) / 20;
+}
+
+float surfaceHeightAtPoint(float x, float y)
+{
+  return coolnoise(x, y) * 20;
+}
+
+const float eps = 0.01;
 
 Color getShadingAtPosition(Vector3 position, Vector3 normal)
 {
-  const Vector3 sun = {1, -0.5, 0};
+  const Vector3 sun = {-1, -0.5, 0};
   const Vector3 v3Intensity = sun - normal;
   const float intensity = dotProduct(v3Intensity, v3Intensity);
   const Color terrainColor = {100, 20, 20};
-  return terrainColor * intensity;
+  return terrainColor * (intensity * intensity / 3);
 }
 
 Vector3 normalAtPosition(Vector3 position)
@@ -95,17 +164,18 @@ Color colorAtTerrainPoint(Ray ray, float hitDistance)
 {
   const Vector3 hitPoint = ray.position + ray.direction * hitDistance;
   const Vector3 normal = normalAtPosition(hitPoint);
-  const Color shasing = getShadingAtPosition(hitPoint, normal);
-  return shasing;
+  const Color shading = getShadingAtPosition(hitPoint, normal);
+  return shading;
 }
 
 bool rayCast(Ray ray, float &hitDistance)
 {
-  const float step = 0.1f;
-  const float minDistance = 0.001f;
-  const float maxDistance = 50.0f;
+  float step = 0.1f;
+  const float minDistance = 10;
+  const float maxDistance = 100.0f;
   for (float t = minDistance; t < maxDistance; t += step)
   {
+    step *= 1.01;
     const Vector3 p = ray.position + ray.direction * t;
     if (p.y < surfaceHeightAtPoint(p.x, p.z))
     {
@@ -132,7 +202,7 @@ Color trace(Ray ray, bool depth)
   return colorAtTerrainPoint(ray, hitDistance);
 };
 
-static int SIZE = 240;
+static int SIZE = 500;
 
 #ifdef __cplusplus
 extern "C"
@@ -166,6 +236,8 @@ extern "C"
         .direction = normalize(eyeVector + xComp + yComp)};
 
     Color result = trace(ray, false);
+    //Color cc = {50, 50, 50};
+    //Color result = cc * noise(x * 0.1, y * 0.1);
 
     switch (index % 4)
     {
